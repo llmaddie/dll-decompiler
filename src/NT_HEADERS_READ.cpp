563f182cb7bd8795ef59a5d2637410d4288b7098
maddie <144840450+llmaddie@users.noreply.github.com>
2025-11-17 15:34:23 +0100

    Update NT_HEADERS_READ.cpp

--- src/NT_HEADERS_READ.cpp
+++ src/NT_HEADERS_READ.cpp
@@ -6,4 +6,57 @@ struct My_IMAGE_FILE_HEADER {
     DWORD   NumberOfSymbols;
     WORD    SizeOfOptionalHeader;
     WORD    Characteristics;
-}
+};
+
+struct My_IMAGE_NT_HEADERS {
+    DWORD Signature; // 0x00004550 ('PE\0\0')
+    My_IMAGE_FILE_HEADER FileHeader; 
+};
+
+struct IMAGE_OPTIONAL_HEADER_COMMON {
+    WORD    Magic;                      // 0x10B (PE32) ou 0x20B (PE32+)
+    BYTE    MajorLinkerVersion;
+    BYTE    MinorLinkerVersion;
+    DWORD   SizeOfCode;
+    DWORD   SizeOfInitializedData;
+    DWORD   SizeOfUninitializedData;
+};
+#pragma pack(push, 1)
+
+struct My_IMAGE_OPTIONAL_HEADER32 {
+    // Adresse (4 octets)
+    DWORD   AddressOfEntryPoint;        // Où commence l'exécution (RVA)
+    DWORD   BaseOfCode;
+    DWORD   BaseOfData;
+
+    // Adresses de base de limage  en mémoire (4 octets)
+    DWORD   ImageBase;      
+    DWORD   SectionAlignment;
+    DWORD   FileAlignment;
+    
+    DWORD   SizeOfImage;                
+    DWORD   NumberOfRvaAndSizes;        // Nombre d'entrées dans le DataDirectory
+    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+};
+
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+
+struct My_IMAGE_OPTIONAL_HEADER64 {
+    ULONGLONG AddressOfEntryPoint;      
+    ULONGLONG BaseOfCode;
+
+    // Adresses de base de l'image en mémoire (8 octets)
+    ULONGLONG ImageBase;              
+    DWORD   SectionAlignment;
+    DWORD   FileAlignment;
+
+    DWORD   SizeOfImage;                
+    // ...
+
+    DWORD   NumberOfRvaAndSizes;       
+    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+};
+
+#pragma pack(pop)
