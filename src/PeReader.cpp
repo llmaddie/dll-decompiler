3a8d1a5e1ad12c5bf20af59508e4da0a42fe4085
llmaddie <contactmaddi3@gmail.com>
2025-11-20 16:50:05 +0100

    Minor change nah its a joke, i've added the EAT EOT, my program take the name of the Rva etc...

--- src/PeReader.cpp
+++ src/PeReader.cpp
@@ -3,5 +3,47 @@
 #include <vector>
 #include <iomanip>
 
-#include "../PeStruct.h"
+#include "header/PeStruct.h"
 
+
+int main() {
+    IMAGE_EXPORT_DIRECTORY exportDir;
+    file.seekg(exportTableFileOffset);
+    file.read((char*)&exportDir, sizeof(exportDir));
+
+    // calcul offset file of the 3 tableaux
+    DWORD FuncAddrOffset = RvaToFileOffset(exportDir.AdressOfFunctions, fileHeader, sections);
+    DWORD namePtrOffset = RvaToFileOffset(exportDir.AdressOfFunctions, fileHeader, sections);
+    DWORD nameOrdinaryOffset = RvaToFileOffset(exportDir.AdressOfFunctions, fileHeader, sections);
+
+    std::cout << "\n--- Exported Functions (" << exportDir.NumberOfNames << "  per name) ---" << std::endl;
+
+    // read the table 
+    std::vector<DWORD> namePointers(exportDir.NumberOfNames);
+    file.seekg(namePtrOffset);
+    file.read((char*)namePointers.data(), exportDir.NumberOfNames *sizeof(WORD));
+
+    // EOT
+    std::vector<WORD> nameOrdinals(exportDir.NumberOfNames);
+    file.seekg(nameOrdinaryOffset);
+    file.read((char*)nameOrdinals.data(), exportDir.NumberOfNames * sizeof(WORD));
+
+    // EAT
+    std::vector<DWORD> functionddresses(exportDir.NumberOfFunctions);
+    file.seekg(funcAddrOffset);
+    file.read((char*)functionAddresses.data(), exportDir.NumberOfFunctions * SIZEOF(DWORD));
+}
+
+// list of name 
+for (DWORD i = 0; i < exportDir.NumberOfNames; ++i) {
+
+
+    // take the name 
+    DWORD nameRVA = namePointers[i];
+    DWORD nameOffset = RvaTOFIleOffset(nameRVA, fileHeader, sections);
+
+    if (nameOffset != 0) {
+        char functionName[256] = {0}; // be careful to have an buffer length necessary
+        file.seekg(nameOffset);
+        file.read(functionName, sizeof(functionName) - 1);
+}
